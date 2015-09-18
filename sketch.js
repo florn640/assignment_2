8a7aa767fc77dbe5000d92fa2c8157b062b7a023
nat1508 <florn640@newschool.edu>
2015-09-18 01:03:25 -0400

    not working
    CRYING

--- sketch.js
+++ sketch.js
@@ -13,35 +13,45 @@ background(167, 213, 242);
 	// set a fill color
 	fill(31, 161, 255);
 
-//var uFo = random(1);
+var uFo = random(1);
 
 	// draw UFO
-//if (uFo > 0.8) {
-		// car
-		fill(107, 112, 105)
-		ellipse(240, 160, 150, 140)
-		ellipse(238, 190, 290, 90)
-	//	ellipse(180, 180, 140, 90)
-		fill(246, 246, 246)
-		ellipse(238, 215, 20, 20)
-		ellipse(155, 208, 20, 20)
-		ellipse(320, 208, 20, 20)
-		fill(255, 244, 24)
-		ellipse(195, 215, 18, 18)
-		ellipse(115, 195, 18, 18)
-		ellipse(363, 195, 18, 18)
-		ellipse(280, 215, 18, 18)
-		fill(22, 23, 21)
-		ellipse(195, 215, 18, 18)
-	//	ellipse(300, 230, 75, 75)
-		//ellipse(180, 230, 75, 75)
+if (random > 0.8) {
+		// drawiing UFO 1
+		fill(107, 112, 105);
+		ellipse(240, 160, 150, 140);
+		ellipse(238, 190, 290, 90);
+		fill(22, 23, 21);
+		ellipse(241, 147, 120, 90);
+		fill(107, 112, 105);
+		ellipse(238, 190, 290, 90);
+		fill(246, 246, 246);
+		ellipse(238, 215, 20, 20);
+		ellipse(155, 208, 20, 20);
+		ellipse(320, 208, 20, 20);
+		fill(255, 244, 24);
+		ellipse(195, 215, 18, 18);
+		ellipse(115, 195, 18, 18);
+		ellipse(363, 195, 18, 18);
+		ellipse(280, 215, 18, 18);
 
-
-
-//	} else {
-	// night
-		//fill(24, 246, 46)
-		//ellipse(250, 130, 50, 50);
+	} else {
+   fill(107, 112, 105);
+   ellipse(260, 180, 150, 140);
+   ellipse(258, 210, 290, 90);
+   fill(22, 23, 21);
+   ellipse(261, 167, 120, 90);
+   fill(107, 112, 105);
+   ellipse(258, 210, 290, 90);
+   fill(246, 246, 246);
+   ellipse(258, 235, 20, 20);
+   ellipse(175, 228, 20, 20);
+   ellipse(340, 228, 20, 20);
+   fill(255, 244, 24);
+   ellipse(215, 235, 18, 18);
+   ellipse(135, 215, 18, 18);
+   ellipse(383, 215, 18, 18);
+   ellipse(300, 235, 18, 18);
 
 
 }
