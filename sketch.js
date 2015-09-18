fce52dc9ac745547283636db01c28886ef6f23b1
nat1508 <florn640@newschool.edu>
2015-09-18 00:51:36 -0400

    draw Ufo

--- sketch.js
+++ sketch.js
@@ -11,61 +11,37 @@ background(167, 213, 242);
 
 
 	// set a fill color
-	fill(242, 242, 242);
+	fill(31, 161, 255);
 
-var backGround = random(1);
+//var uFo = random(1);
 
-	// draw background
-if (backGround < 0.8) {
-		// day
-		background(13, 12, 12);
-		fill(12, 12, 12);
-
-	} else {
-		// night
-		background(200, 20, 40);
-		fill(10, 30, 160);
-
-	}
-
-
-	// draw sun
-	if (backGround < 0.5) {
-		fill(280, 20, 90);
-		ellipse(random(320, 640), random(0, 280), 100, 100);
-	}
-
-	// draw stars
-	if (backGround > 0.5) {
-		fill(250, 250, 250);
-		var starSize;
-		starSize = random(4, 8);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-		starSize = random(2, 5);
-		ellipse(random(0, 640), random(0, 280), starSize, starSize);
-	}
-
-	// draw tree
-	var treeX = random(0, 320);
-	if (backGround< 0.5) {
-		fill(0, 150, 0);
-	} else {
-		fill(0, 80, 0);
-	}
-	triangle(treeX, 300, treeX - 50, 425, treeX + 50, 425);
-	triangle(treeX, 240, treeX - 30, 325, treeX + 30, 325);
+	// draw UFO
+//if (uFo > 0.8) {
+		// car
+		fill(107, 112, 105)
+		ellipse(240, 160, 150, 140)
+		ellipse(238, 190, 290, 90)
+	//	ellipse(180, 180, 140, 90)
+		fill(246, 246, 246)
+		ellipse(238, 215, 20, 20)
+		ellipse(155, 208, 20, 20)
+		ellipse(320, 208, 20, 20)
+		fill(255, 244, 24)
+		ellipse(195, 215, 18, 18)
+		ellipse(115, 195, 18, 18)
+		ellipse(363, 195, 18, 18)
+		ellipse(280, 215, 18, 18)
+		fill(22, 23, 21)
+		ellipse(195, 215, 18, 18)
+	//	ellipse(300, 230, 75, 75)
+		//ellipse(180, 230, 75, 75)
 
 
-	noLoop();
+
+//	} else {
+	// night
+		//fill(24, 246, 46)
+		//ellipse(250, 130, 50, 50);
+
 
 }
