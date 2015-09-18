0638ce65722ebe4786885367caec03dd703f6017
nat1508 <florn640@newschool.edu>
2015-09-18 12:16:28 -0400

    error fixed

--- sketch.js
+++ sketch.js
@@ -54,13 +54,7 @@ function draw() {
 			ellipse(425, 275, 18, 18);
 			ellipse(673, 275, 18, 18);
 			ellipse(590, 295, 18, 18);
-    }
-
-		var aLiens = random(0,1);
-		console.log("The random aLiens value is: " + aLiens);
-			//drawing Alien 1
-		if (aLiens < 0.5) {
-			fill(30, 199, 24);
+     	fill(30, 199, 24);
 			ellipse(555, 70, 30, 40);
 			ellipse(555, 115, 40, 60);
 			ellipse(545, 140, 10, 40);
@@ -97,16 +91,8 @@ function draw() {
 			fill(168, 176, 164);
 			ellipse(348, 66, 5, 5);
 			ellipse(362, 66, 5, 5);
-			} else {
-				fill(22, 23, 21);
-				ellipse(448, 66, 21, 25);
-				ellipse(362, 66, 21, 25);
-				ellipse(356, 80, 24, 19);
-				fill(168, 176, 164);
-				ellipse(348, 66, 15, 15);
-				ellipse(362, 66, 15, 15);
 
 		}
-	
+
 		noLoop();
 }
